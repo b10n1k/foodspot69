28f0137af333b8091a7ff1c2fb071419aad420c6
b10n1k <jbonatakis@gmail.com>
2014-03-17 01:00:12 +0200

    add ajax function

--- static/foodspot/js/foodspot69.js
+++ static/foodspot/js/foodspot69.js
@@ -1,17 +1,17 @@
 $(document).ready( function(){
     $(".orderbtn").click(function(){
-            p=$(this).prop("id");
-            $.ajax({
-                type:"GET",
-                url:"order/",
-                data:{"itemId":p
-                     //'csrfmiddlewaretoken': $("{% csrf_token %}")
-                     },
-                success: function(data){
-                    $('#selected').html("data.title");
-                }
-
-            });
-
+        p=$(this).prop("id");
+        $.ajax({
+            type:"GET",
+            url:"order/",
+            data:{"itemId":p
+                  //'csrfmiddlewaretoken': $("{% csrf_token %}")
+                 },
+            success: function(data){
+                $('#selected').html("data.title");
+            }
+            
+        });
+        
     });
 });
