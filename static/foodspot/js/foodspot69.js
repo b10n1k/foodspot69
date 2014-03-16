ba676a404c693ecbac7dfbfcefbb61c754960e5a
b10n1k <jbonatakis@gmail.com>
2014-03-17 00:57:32 +0200

    add ajax function

--- static/foodspot/js/foodspot69.js
+++ static/foodspot/js/foodspot69.js
@@ -1,18 +1,18 @@
 $(document).ready( function(){
-    $(".orderbtn").click(function(){	
-	    p=$(this).prop("id");
-	    $.ajax({
-		type:"GET",
-		url:"order/",
-		data:{"itemId":p
-		     //'csrfmiddlewaretoken': $("{% csrf_token %}") 
-		     },
-		success: function(data){
-                    $('#selected').html(data);
+    $(".orderbtn").click(function(){    
+            p=$(this).prop("id");
+            $.ajax({
+                type:"GET",
+                url:"order/",
+                data:{"itemId":p
+                     //'csrfmiddlewaretoken': $("{% csrf_token %}") 
+                     },
+                success: function(data){
+                    $('#selected').html("data.title");
                 }
-		
-	    });
-	    	
+                
+            });
+                
     });
 });
 
